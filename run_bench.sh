c6067f954fb6b3e98f086a47d218af7c3f0e3b99
Rafael Almeida Batista <almeidabatista@gmail.com>
2013-04-09 08:01:29 -0300

    Atualizando script de benchmark

--- run_bench.sh
+++ run_bench.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-glpsol --model restaurantes.mod --data bench.data --output /dev/null | grep "[Time|Memory] used"
+python instance_generator.py 10 10 > instancias/bench.data
+glpsol --model restaurantes.mod --data instancias/bench.data --output 
+/dev/null | grep "[Time|Memory] used"
