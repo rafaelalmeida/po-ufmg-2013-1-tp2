35f66e4bb3b4a61d4fabdf533ba983b24be86fbe
Rafael Almeida Batista <almeidabatista@gmail.com>
2013-04-10 20:18:31 -0300

    Resultados da solução do exemplo

--- run_example.sh
+++ run_example.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-glpsol --model restaurante.mod --data enunciado.data --output /dev/stdout --ranges sensivity.txt
+glpsol --model restaurante.mod --data enunciado.data --output results/solution.txt --ranges results/sensivity.txt
